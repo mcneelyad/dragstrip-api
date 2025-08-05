0dc30b72b7304ad291efdf9a412667675c1d1ab7
mcneelyad <36091040+mcneelyad@users.noreply.github.com>
2025-08-05 15:15:00 -0400

    Remove hardcoded port value

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.get("/location", (req, res) => {
     }
 });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("Server is running on port 5000");
 });
 
